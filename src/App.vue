cf0527c5326e6b2febb0b034d933a6ba328f9a7d
belajar-vue-2 <aryaadi229@gmail.com>
2024-06-20 13:13:37 +0700

    Menggunakan Routerlink Untuk Berpindah Halaman

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { RouterView } from "vue-router";
+import navbar from "./components/navbar.vue";
 </script>
 
 <template>
+  <navbar></navbar>
   <RouterView />
 </template>
---
+++ src/components/navbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav>
+    <a href="/">Home</a>
+    <br />
+    <a href="/about">About</a>
+    <hr />
+    <router-link to="/">Home</router-link>
+    <br />
+    <router-link to="/about">About</router-link>
+    <hr />
+  </nav>
+</template>
