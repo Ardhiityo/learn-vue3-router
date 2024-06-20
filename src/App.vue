309120d498750738f25556d3ebf0fc23384dd83f
belajar-vue-2 <aryaadi229@gmail.com>
2024-06-20 17:54:43 +0700

    Membuka Halaman Dengan Url Path Dinamis

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,25 @@
 <script setup>
 import navbar from "./components/navbar.vue";
+import products from "./data/index.json";
 </script>
 
 <template>
   <navbar></navbar>
+  <div class="products">
+    <div v-for="product in products" :key="product.id">
+      <router-link :to="`/product/${product.id}`">
+        <p>{{ product.item }}</p>
+      </router-link>
+    </div>
+  </div>
   <RouterView />
 </template>
+
+<style scoped>
+.products {
+  display: flex;
+  flex-wrap: wrap;
+  gap : 10px;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,17 +4,23 @@ import {
 } from 'vue-router'
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(
+        import.meta.env.BASE_URL),
     routes: [{
-        path: '/',
-        name: 'home',
+            path: '/',
+            name: 'home',
             component: () => import('../views/Home.vue')
         },
         {
             path: '/about',
             name: 'about',
             component: () => import('../views/About.vue')
-        }
+        },
+        {
+            path: '/product/:id',
+            name: 'product',
+            component: () => import('../views/Product.vue')
+        },
     ]
 })
 
