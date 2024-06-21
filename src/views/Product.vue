7893680bc63e8e02e89e1dc416ef70999b843093
belajar-vue-2 <aryaadi229@gmail.com>
2024-06-21 19:27:18 +0700

    Mengubah Route Melalui Kode Program

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -1,16 +1,22 @@
 <script setup>
-import { useRoute} from "vue-router";
+import { useRoute, useRouter } from "vue-router";
+
 const route = useRoute();
 import data from "../data/index.json";
 const product = data.find((res) => res.id === parseInt(route.params.id));
-</script>
 
+const router = useRouter();
+function showOwner() {
+  router.push({ name: "owner", params: { id: parseInt(route.params.id) } });
+}
+</script>
 
 <template>
   <div>
     <h1>Products</h1>
     <h2>id : {{ product.id }}</h2>
     <p>{{ product.item }}</p>
+    <button @click="showOwner">Show Owner</button>
     <RouterView />
   </div>
 </template>
